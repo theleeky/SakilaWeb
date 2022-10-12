169f3e284911df71ee22e0e78844fe93eb05a535
Malek <malekbusari@gmail.com>
2022-10-12 12:23:05 +0100

    Add sessions and ability to add actors to castList

--- src/main/java/com/sparta/sakilaweb/controller/SakilaWebController.java
+++ src/main/java/com/sparta/sakilaweb/controller/SakilaWebController.java
@@ -1,28 +1,38 @@
 package com.sparta.sakilaweb.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.sakilaweb.dao.ActorDAO;
+import com.sparta.sakilaweb.dto.ActorDTO;
 import com.sparta.sakilaweb.entity.Actor;
 import com.sparta.sakilaweb.repo.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@SessionAttributes("castList")
 public class SakilaWebController {
     @Autowired
     private ActorRepository repo;
+    private final ActorDAO actorDAO;
+    private ObjectMapper mapper;
+    private HttpHeaders headers;
 
-    @GetMapping("/sakila/testname")
-    public String testName(String nameParam, Model model){
-        model.addAttribute("nameAtribute", nameParam+" is awesome");
-        return "displayName";
+    public SakilaWebController(ActorDAO actorDAO, ObjectMapper mapper) {
+        this.actorDAO = actorDAO;
+        mapper = new ObjectMapper();
     }
+
+//    @GetMapping("/sakila/testname")
+//    public String testName(String nameParam, Model model){
+//        model.addAttribute("nameAtribute", nameParam+" is awesome");
+//        return "displayName";
+//    }
 
     @GetMapping("/sakila/actor")
     public String findActor(int id, Model model){
@@ -31,11 +41,11 @@ public class SakilaWebController {
         return "displayActor";
     }
 
-    @GetMapping("sakila/actor/all")
+    @GetMapping("/sakila")
     public String findAllActors(Model model){
-        List<Actor> allActors = repo.findAll();
+        List<ActorDTO> allActors = actorDAO.getAllActors();
         model.addAttribute("allActors", allActors);
-        return "displayAllActor";
+        return "index";
     }
 
     @GetMapping("sakila/actor/id/{id}")
@@ -54,17 +64,47 @@ public class SakilaWebController {
 
     @PostMapping("sakila/actor/form")
     public String createActor(@ModelAttribute Actor actor, Model model){
-        actor.setLastUpdate(Instant.now());
-        repo.save(actor);
-        Actor newActor = repo.findById(actor.getId()).get();
-        model.addAttribute("actor", newActor);
+        System.out.println(actor.getFirstName());
+        ActorDTO savedActor = actorDAO.addNewActor(actor);
+        model.addAttribute("actor", savedActor);
         return "displayActor";
     }
 
-    @PostMapping("sakila/actor/form/{id}")
-    public String updateActor(@ModelAttribute Actor actor, @PathVariable int id, Model model){
-        Actor actorToUpdate = repo.findById(id).get();
-
+    @GetMapping("sakila/actor/delete/{id}")
+    public String deleteActor(@PathVariable int id, Model model){
+        ActorDTO actor = actorDAO.getActorById(id);
+        model.addAttribute("actor", actor);
+        return "actorToDelete";
     }
+
+    @PostMapping("sakila/actor/delete/{id}")
+    public String actorDeleted(@ModelAttribute @PathVariable(name = "id") int id, Model model){
+        System.out.println(id);
+        int deletedActor = actorDAO.deleteActor(id);
+        model.addAttribute("actor", deletedActor);
+        return "actorDeleted";
+    }
+
+    @GetMapping("/sakila/cast/add/{id}")
+    public String addCastMember(@PathVariable int id,
+                                Model model,
+                                @ModelAttribute("castList") List<ActorDTO> castList){
+        ActorDTO actorToAdd = actorDAO.getActorById(id);
+        if (!castList.contains(actorToAdd))
+            castList.add(actorToAdd);
+        System.out.println(castList);
+        return "displayCastList";
+    }
+
+    @ModelAttribute("castList")
+    public List<ActorDTO> castList(){
+        System.out.println("Initializing the cast list...");
+        return new ArrayList<>();
+    }
+
+//    @PostMapping("sakila/actor/form/{id}")
+//    public String updateActor(@ModelAttribute Actor actor, @PathVariable int id, Model model){
+//        Actor actorToUpdate = repo.findById(id).get();
+//    }
 
 }
